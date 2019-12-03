5a89197da9a1543678092da8660a1ddbe420cae8
katya <ket11141@gmail.com>
2019-12-03 21:13:56 +0200

    composer update

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,13 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+        $user->getFirstName('Kate');
+        $user->getSecondName('Balan');
+        $user->getAge(22);
         // $product = new Product();
         // $manager->persist($product);
 
